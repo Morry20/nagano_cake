d76bce8c1a34475e5a480a0660af6e4e0760c883
EC2 Default User <ku.l.taf54@gmail.com>
2021-10-24 09:05:23 +0000

    [finish]注文履歴一覧

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,5 @@
 class Customer < ApplicationRecord
    enum is_deleted: { "有効": true, "退会": false }
+   
+   has_many :orders, dependent: :destroy
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,8 @@
 class Item < ApplicationRecord
 
    belongs_to :genre
+   
+   has_many :order_details, dependent: :destroy
 
    attachment :image
 
