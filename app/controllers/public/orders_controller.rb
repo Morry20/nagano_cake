f191623f7e8999e3b4dd396d32c1646dddc6fcc3
EC2 Default User <ku.l.taf54@gmail.com>
2021-11-18 08:48:41 +0000

    [almost]注文情報確認

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -6,31 +6,49 @@ class Public::OrdersController < ApplicationController
   end
 
   def confirm
-    #binding.pry
-    @order = Order.new(order_params)
+    @order = Order.new
     @cart_items = current_customer.cart_items
     @order.shipping_cost = 800
     @order.payment_method = params[:order][:payment_method]
-    if params[:order][:select_address] == 0
+    if params[:order][:select_address] == "0"
       @order.postal_code = current_customer.postal_code
       @order.address = current_customer.address
       @order.name = current_customer.first_name + current_customer.last_name
-    elsif params[:order][:select_address] == 1
-      @order.postal_code = current_customer.postal_code
-      @order.address = current_customer.address
-      @order.name = current_customer.first_name + current_customer.last_name
-    elsif params[:order][:select_address] == 2
+    elsif params[:order][:select_address] == "1"
+      @address = Address.find(params[:order][:current_customer_address_id])
+      @order.postal_code = @address.postal_code
+      @order.address = @address.address
+      @order.name = @address.name
+    elsif params[:order][:select_address] == "2"
       @order.postal_code = current_customer.postal_code
       @order.address = current_customer.address
       @order.name = current_customer.first_name + current_customer.last_name
     end
-    
+    # binding.pry
   end
 
   def complete
   end
 
   def create
+    @order = Order.new(order_params)
+    @cart_items = current_customer.cart_items
+    #@order = Order.new(order_params)
+    @order.customer_id = current_customer.id
+    @order.save
+    item = []
+    @cart_items.each do |i|
+      item << @order.order_details.new(
+        order_id: @order.id,
+        item_id: i.item_id,
+        price: i.item.price,
+        amount: i.amount
+        )
+
+    end
+    OrderDetail.import(item)
+#カートの削除機能をつける
+    redirect_to orders_complete_path
   end
 
   def index
@@ -41,8 +59,9 @@ class Public::OrdersController < ApplicationController
   end
 
   private
+
   def order_params
-      params.require(:order).permit(:payment_method, :postal_code, :address, :name)
+    params.require(:order).permit(:payment_method, :postal_code, :address, :name, :shipping_cost, :total_payment)
   end
 
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,9 +1,11 @@
 class Order < ApplicationRecord
-  
-  belongs_to :customer
-  
+
+  belongs_to :customer, optional: :true
+
   has_many :order_details, dependent: :destroy
-  
+
   enum payment_method: { credit_card: 0, transfer: 1 }
-  
+
+
+
 end
