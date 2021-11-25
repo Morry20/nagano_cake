4da8727a89fded77066ce33ab20d0fc6e38b838e
EC2 Default User <ku.l.taf54@gmail.com>
2021-11-25 09:44:03 +0000

    [kaminari]完了

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,14 +1,14 @@
 class Public::HomesController < ApplicationController
-  
+
   def top
     @genres = Genre.all
-    @items = Item.all
+    @items = Item.page(params[:page]).reverse_order
     @items_count = Item.count
   end
-  
+
   def about
   end
-  
-  
-  
+
+
+
 end
