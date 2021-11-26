e4324b196930d3122e8471cc3266c7f59855d486
EC2 Default User <ku.l.taf54@gmail.com>
2021-11-26 12:54:21 +0000

    [layout]almost

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,25 @@
 class ApplicationController < ActionController::Base
-   
+
   before_action :configure_permitted_parameters, if: :devise_controller?
-  
-  def after_sign_in_path_for(resource) 
-    public_customers_my_page_path
+
+  def after_sign_in_path_for(resource)
+    if admin_signed_in?
+      admin_path
+    elsif customer_signed_in?
+      public_customers_my_page_path
+    end
   end
-  
+
+  def after_sign_out_path_for(resource)
+    new_admin_sign_in_path
+  end
+
   private
 
     #sin up時の登録情報追加
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name,:first_name,:last_name_kana,:first_name_kana,:postal_code,:address,:telephone_number])
     end
-   
-   
+
+
 end
